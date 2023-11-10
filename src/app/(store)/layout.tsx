9c7ca28b71e611a2e7a50a91584ee121909a5a92
Rodrigo <rodrigosanyy@gmail.com>
2023-11-10 11:56:07 -0300

    add header

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -3,9 +3,9 @@ import { ReactNode } from 'react'
 
 export default function StoreLayout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <div className="grid-rows-app mx-auto grid min-h-screen w-full max-w-[1600px] gap-5 p-8">
       <Header />
       {children}
-    </>
+    </div>
   )
 }
