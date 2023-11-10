13f85040e17d90784bb42f40ea0ea7dd8bca08d3
Rodrigo <rodrigosanyy@gmail.com>
2023-11-10 11:01:12 -0300

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      options: {
+        plugins: ['prettier-plugin-tailwindcss'],
+      },
+    },
+  ],
+}
