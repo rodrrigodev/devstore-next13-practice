c7e847ed7dcad5340a2fbac01404d3f3984f938c
Rodrigo <rodrigosanyy@gmail.com>
2023-11-13 16:43:10 -0300

    add context

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -31,6 +31,17 @@ export async function generateMetadata({
   }
 }
 
+export async function generateStaticParams() {
+  const response = await api('/products/featured')
+  const products: Product[] = await response.json()
+
+  // return [{slug: 'moletom-never-stop-learning}]
+
+  return products.map((product) => {
+    return { slug: product.slug }
+  })
+}
+
 export default async function ProductPage({ params: { slug } }: ProductProps) {
   const product = await getProduct(slug)
   return (
