c3438585c496973b5c4e9d8f0d81a4c03891bf38
Rodrigo <rodrigosanyy@gmail.com>
2023-11-14 15:07:40 -0300

    add new functionality

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -1,39 +1,73 @@
+import { api } from '@/app/data/api'
+import { Product } from '@/app/data/types/product'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
-export default async function Search() {
+interface SearchProps {
+  searchParams: {
+    q: string
+  }
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
+  const response = await api(`/products/search?q=${query}`, {
+    next: {
+      revalidate: 60 * 60, // 1hour
+    },
+  })
+  const products = await response.json()
+  return products
+}
+
+export default async function Search({
+  searchParams: { q: query },
+}: SearchProps) {
+  if (!query) {
+    redirect('/')
+  }
+
+  const products = await searchProducts(query)
+
   return (
     <div className="flex flex-col gap-4">
       <p className="text-sm">
-        Resultados para: <span className="font-semibold">moletom</span>
+        Resultados para: <span className="font-semibold">{query}</span>
       </p>
 
       <div className="grid grid-cols-3 gap-6">
-        <Link
-          href={`/product/moletom`}
-          className="group relative flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-        >
-          <Image
-            src="/moletom"
-            className="transition-transform duration-500 group-hover:scale-105"
-            width={480}
-            height={480}
-            quality={100}
-            alt="abc"
-          />
+        {products.map((product) => {
+          return (
+            <Link
+              href={`/product/${product.slug}`}
+              key={product.id}
+              className="group relative flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+            >
+              <Image
+                src={product.image}
+                className="transition-transform duration-500 group-hover:scale-105"
+                width={480}
+                height={480}
+                quality={100}
+                alt="abc"
+              />
 
-          <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-            <span className="truncate text-sm">moletom</span>
-            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-              {Number(199).toLocaleString('pt-BR', {
-                style: 'currency',
-                currency: 'BRL',
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              })}
-            </span>
-          </div>
-        </Link>
+              <div className="absolute bottom-10 right-10 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                <span className="truncate text-sm">{product.title}</span>
+                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                  {product.price.toLocaleString('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                    minimumFractionDigits: 0,
+                    maximumFractionDigits: 0,
+                  })}
+                </span>
+              </div>
+            </Link>
+          )
+        })}
       </div>
     </div>
   )
