a4e05b488a5f26d713dab885770ab8cf086a1e78
Rodrigo <rodrigosanyy@gmail.com>
2023-11-15 09:33:17 -0300

    new features in cypress

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -1,7 +1,9 @@
 describe('add product to cart', () => {
-  it('should be able to navigate to the product page and add it to the cart', () => {
-    cy.visit('http://localhost:3000/')
+  beforeEach(() => {
+    cy.visit('/')
+  })
 
+  it('should be able to navigate to the product page and add it to the cart', () => {
     cy.get('a[href^="/product"]').first().click()
 
     cy.url().should('include', '/product')
@@ -14,8 +16,6 @@ describe('add product to cart', () => {
   })
 
   it('should not count duplicated products on cart', () => {
-    cy.visit('http://localhost:3000/')
-
     cy.get('a[href^="/product"]').first().click()
 
     cy.url().should('include', '/product')
@@ -29,9 +29,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.visit('http://localhost:3000/')
-
-    cy.get('input[name=q]').type('moletom').parent('form').submit()
+    cy.searchByQuery('moletom')
 
     cy.get('a[href^="/product"]').first().click()
 
